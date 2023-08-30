7aadb31d9da1bfddad56d47d79c7894de900a230
Mintti <mintti97@gmail.com>
2023-08-31 03:38:24 +0900

    Status 초기값 설정 및 배틀 이벤트 개선

--- Assets/Scripts/Event/BattleEvent.cs
+++ Assets/Scripts/Event/BattleEvent.cs
@@ -1,89 +1,76 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleEvent : MonoBehaviour
 {
+    private GameManager _gameManager;
     private UIManager _uiManager;
     private Knight _knight;
     private Monster _monster;
 
+    public Image img;
+
     public void Init(Knight knight, Monster monster)
     {
+        _gameManager ??= GameObject.Find(nameof(GameManager)).GetComponent<GameManager>(); 
         _knight = knight;
         _monster = monster;
+        img.sprite = _monster.Sprite;
     }
 
     public void Execute()
     {
-        CreatTemp();
-
         _uiManager = GameObject.Find(nameof(UIManager)).GetComponent<UIManager>();
 
-        _uiManager.CombatActive(_monster.Sprite);
+        _uiManager.CombatActive();
         StartCoroutine(Battle());
     }
-    //test
-    private void CreatTemp()
-    {
-        _knight = new Knight()
-        {
-            Status = new Status()
-            {
-                MaxHp = 15,
-                CurrentHp = 10,
-                Power = 1
-            }
-        };
-
-        _monster = new Monster()
-        {
-            Name = "<color=#FF0000>고블린</color>",
-            Status = new Status()
-            {
-                MaxHp = 5,
-                Power = 3
-            }
-        };
-    }
-
+    
     IEnumerator Battle()
     {
         bool knightTurn = true;
         bool monsterTurn = false;
-        int currentPlayerHP = _knight.Status.CurrentHp;
-        int currentMonsterHP = _monster.Status.MaxHp;
+        ;
 
-        while (currentPlayerHP > 0 && currentMonsterHP > 0)
+        while (_knight.Status.CurrentHp > 0 &&  _monster.Status.MaxHp > 0)
         {
             if (knightTurn)
             {
-                currentMonsterHP -= _knight.Status.Power;
-                _uiManager.OutputCombatText("<color=#33FF33>용사</color>", _monster.Name, _knight.Status.Power, currentMonsterHP);
+                _monster.Status.MaxHp -= _knight.Status.Power;
+                _uiManager.OutputCombatText("<color=#33FF33>용사</color>", _monster.Name, _knight.Status.Power,  _monster.Status.MaxHp);
+                
                 knightTurn = false;
                 monsterTurn = true;
+                
+                if ( _monster.Status.MaxHp <= 0)
+                {
+                    _uiManager.CombatPlayerWinText(_monster.Name);
+                    _knight.Status.CurrentHp = _knight.Status.CurrentHp;
+                    _uiManager.combatPanelExitButton.SetActive(true);
+                    _gameManager.EventPrinting = false;
+                    yield break;
+                }
             }
             else if (monsterTurn)
             {
-                currentPlayerHP -= _monster.Status.Power;
-                _uiManager.OutputCombatText(_monster.Name, "<color=#33FF33>용사</color>", _monster.Status.Power, currentPlayerHP);
+                _knight.Status.CurrentHp -= _monster.Status.Power;
+                _uiManager.OutputCombatText(_monster.Name, "<color=#33FF33>용사</color>", _monster.Status.Power, _knight.Status.CurrentHp);
+                
                 monsterTurn = false;
                 knightTurn = true;
+                
+                if (_knight.Status.CurrentHp <= 0)
+                {
+                    _uiManager.CombatMonsterWinText();
+                    yield return new WaitForSeconds(2f);
+                    _uiManager.gameOverButton.SetActive(true);
+                    _gameManager.EventPrinting = false;
+                    yield break;
+                }
             }
 
             yield return new WaitForSeconds(0.5f);
-        }
-
-        if (currentPlayerHP <= 0)
-        {
-            _uiManager.CombatMonsterWinText();
-            yield return new WaitForSeconds(2f);
-            _uiManager.gameOverButton.SetActive(true);
-        }
-        else if (currentMonsterHP <= 0)
-        {
-            _uiManager.CombatPlayerWinText(_monster.Name);
-            _knight.Status.CurrentHp = currentPlayerHP;
-            _uiManager.combatPanelExitButton.SetActive(true);
         }
     }
 
